89056290d37332f515e06e1684eeb7294243a9ac
Luis Gustavo Piai <piaizete123@gmail.com>
2022-04-06 11:55:44 -0300

    contact url removed

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from recipes.views import contato_view, home_view, sobre_view
+from recipes.views import home_view
 
 urlpatterns = [
     path('', home_view),
-    path('sobre/', sobre_view),
-    path('contato/', contato_view)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,11 +8,3 @@ def home_view(request):
         'name': 'Luis Gustavo',
         'name2': 'Layla'
     })
-
-
-def sobre_view(request):
-    return HttpResponse('SOBRE 1')
-
-
-def contato_view(request):
-    return HttpResponse('CONTATO 1')
